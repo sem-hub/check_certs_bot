dee8f5d8ba97b203153c17a6c63d7eb1382b45e4
Sergey Matveychuk <sem@semmy.ru>
2020-11-10 20:36:47 +0300

    Get rid of python2'izm, More debug info, escape_markdown where need. More fixes with a new check_cert.py

--- periodically_check.py
+++ periodically_check.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
-from __future__ import print_function
 import argparse
 import re
 import rpyc
 import sqlite3
 import subprocess
-from os import path
+import sys
+from os import sys, path
+
+work_dir = path.dirname(path.abspath(__file__))
+sys.path.append(work_dir)
+
+from escape_markdown import escape_markdown
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--debug', action='store_true')
@@ -19,35 +24,44 @@ cur = con.cursor()
 cur.execute('SELECT * FROM servers')
 for r in cur.fetchall():
     if debug:
-        print('%s %s %s' % (r[1], r[2], r[3]))
+        print('*** %s %s %s' % (r[1], r[2], r[3]))
     old_cert_id = r[8]
     if r[7] == 'HOLD':
         if debug:
-            print('Skipped')
+            print('*** Skipped')
         continue
+    result = ''
+    if debug:
+        print('***Command to run:\n%s %s %s %s %s %s %s %s %s' % ('/usr/bin/python3', prog_dir+'/check_certs.py', '--quiet', '--print-id', '--warn-before-expired', r[5], r[1], r[2], r[3]))
     try:
         result = subprocess.check_output(['/usr/bin/python3', prog_dir+'/check_certs.py', '--quiet', '--print-id', '--warn-before-expired', r[5], r[1], r[2], r[3]], stderr=subprocess.STDOUT)
     except:
-        result = b''
-
-    m = re.search(b'ID: ([0-9A-Z]+)\n', result)
-    if m == None:
-        rcon = rpyc.connect('localhost', 18861)
-        rcon.root.add_message(r[4], '%s %s %s check certificate error:\n%s' % (r[1], r[2], r[3], result.decode('utf-8')))
+        result = 'Check command failure'
         if debug:
-            print('%s' % result.decode('utf-8'))
-        continue
-    cert_id = m.group(1)
-    result = re.sub('ID: ([0-9A-Z]+)\n', '', result.decode('utf8'))
+            print('***subprocess failure: %s' % sys.exc_info()[0])
+
+    if type(result) == bytes:
+        result = result.decode('utf-8')
     if result[-1:] == '\n':
         result = result[:len(result)-1]
+    m = re.search('ID: ([0-9A-Z]+)\n?', result)
+    if m == None:
+        rcon = rpyc.connect('localhost', 18861)
+        rcon.root.add_message(r[4], '%s %s %s check certificate error:\n%s' % (r[1], r[2], r[3], result))
+        if debug:
+            print('***Error: |%s|' % result)
+        cur.execute('UPDATE servers SET last_checked=CURRENT_TIMESTAMP, status=? WHERE hostname=? AND port=?', (escape_markdown(result), r[1], r[3]))
+        con.commit()
+        continue
+    cert_id = m.group(1)
+    result = re.sub('ID: ([0-9A-Z]+)\n?', '', result)
     if result != '':
         cur.execute('UPDATE servers SET last_checked=CURRENT_TIMESTAMP, status=?, cert_id=? WHERE hostname=? AND port=?', (result, cert_id, r[1], r[3]))
         con.commit()
         rcon = rpyc.connect('localhost', 18861)
         rcon.root.add_message(r[4], '%s %s %s check certificate error:\n%s' % (r[1], r[2], r[3], result))
         if debug:
-            print('%s' % result)
+            print('***Error*: %s' % result)
     else:
         if cert_id == old_cert_id:
             result = 'OK'
@@ -56,6 +70,6 @@ for r in cur.fetchall():
         cur.execute('UPDATE servers SET last_checked=CURRENT_TIMESTAMP, status=?, cert_id=?  WHERE hostname=? AND port=?', (result, cert_id, r[1], r[3]))
         con.commit()
         if debug:
-            print(result)
+            print('***%s' % result)
 
 con.close()
