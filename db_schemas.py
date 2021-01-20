3f74776f571927c8d39dac13ebde647fe185f5e6
Sergey Matveychuk <sem@semmy.ru>
2021-01-20 23:03:42 +0300

    New DB schemas for users and activity

--- db_schemas.py
+++ db_schemas.py
@@ -1,12 +1,20 @@
 users_create_statement='''CREATE TABLE IF NOT EXISTS users (
+id TEXT,
 name TEXT,
-chat_id TEXT
+full_name TEXT,
+first_met TEXT,
+last_activity TEXT
 )'''
 
-periodic_create_statement='''CREATE TABLE IF NOT EXISTS periodic (
+activity_create_statement='''CREATE TABLE IF NOT EXISTS activity (
+id INTEGER AUTOINCREMENT,
+user_id TEXT,
+cmd TEXT,
+when TEXT
 )'''
 
 servers_create_statement='''CREATE TABLE IF NOT EXISTS servers (
+id INTEGER AUTOINCREMENT,
 when_added TEXT,
 url TEXT,
 chat_id TEXT,
