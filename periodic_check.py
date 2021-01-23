2975326bb3b97551e48691d5be6f096b728080ee
Sergey Matveychuk <sem@semmy.ru>
2021-01-24 01:07:35 +0300

    Run all work parallelly. Before only certificate checks run parallelly and parsing results coherently.

--- periodic_check.py
+++ periodic_check.py
@@ -33,7 +33,43 @@ def proc_exec(rt: tuple) -> dict:
     res['url'] = r['url']
     res['chat_id'] = r['chat_id']
     res['out_text'] = check_cert(r['url'], flags)
+    if not dry_run:
+        process_results(res)
     return res
+
+def process_results(r: dict):
+    global db_factory
+    global servers_db
+    global dry_run
+
+    if not r:
+        return
+    result = r['out_text']
+    if type(result) == bytes:
+        result = result.decode('utf-8')
+    if result[-1:] == '\n':
+        result = result[:len(result)-1]
+    m = re.search('ID: ([0-9A-Z]+)\n?', result)
+    if m == None:
+        send_to_chat(r['chat_id'], f'{r["url"]} check certificate error:\n{result}')
+        logging.debug(f'Error: |{result}|')
+        servers_db.update(f'last_checked=CURRENT_TIMESTAMP, status="{escape_markdown(result)}"', f'url="{r["url"]}" AND chat_id="{r["chat_id"]}"')
+        return
+    cert_id = m.group(1)
+    result = re.sub('ID: ([0-9A-Z]+)\n?', '', result)
+    if result != '':
+        send_to_chat(r['chat_id'], f'{r["url"]} check certificate error:\n{result}')
+        logging.debug(f'Error*: {result}')
+        servers_db.update(f'last_checked=CURRENT_TIMESTAMP, status="{escape_markdown(result)}", cert_id="{cert_id}"',  f'url="{r["url"]}" AND chat_id="{r["chat_id"]}"')
+    else:
+        # It;s a first check or certificate did not changed
+        if r['cert_id'] == '0' or cert_id == r['cert_id']:
+            result = 'OK'
+        else:
+            result = 'Certificate was changed'
+            send_to_chat(r['chat_id'], f'{r["url"]} check certificate:\n{result}')
+        logging.debug(f'{result}')
+        servers_db.update(f'last_checked=CURRENT_TIMESTAMP, last_ok=CURRENT_TIMESTAMP, status="{escape_markdown(result)}", cert_id="{cert_id}"',  f'url="{r["url"]}" AND chat_id="{r["chat_id"]}"')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -47,41 +83,15 @@ def main():
     else:
         logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-    proc_num = args.proc_num
+    global db_factory
+    global servers_db
+    global dry_run
+    dry_run = args.dry_run
     db_factory = DB_factory()
     servers_db = db_factory.get_db('servers')
     res = servers_db.select('*')
-    with Pool(processes=proc_num) as pool:
+    with Pool(processes=args.proc_num) as pool:
         pres = pool.map(proc_exec, enumerate(res))
-    for r in pres:
-        if args.dry_run or not r:
-            continue
-        result = r['out_text']
-        if type(result) == bytes:
-            result = result.decode('utf-8')
-        if result[-1:] == '\n':
-            result = result[:len(result)-1]
-        m = re.search('ID: ([0-9A-Z]+)\n?', result)
-        if m == None:
-            send_to_chat(r['chat_id'], f'{r["url"]} check certificate error:\n{result}')
-            logging.debug(f'Error: |{result}|')
-            servers_db.update(f'last_checked=CURRENT_TIMESTAMP, status="{escape_markdown(result)}"', f'url="{r["url"]}" AND chat_id="{r["chat_id"]}"')
-            continue
-        cert_id = m.group(1)
-        result = re.sub('ID: ([0-9A-Z]+)\n?', '', result)
-        if result != '':
-            send_to_chat(r['chat_id'], f'{r["url"]} check certificate error:\n{result}')
-            logging.debug(f'Error*: {result}')
-            servers_db.update(f'last_checked=CURRENT_TIMESTAMP, status="{escape_markdown(result)}", cert_id="{cert_id}"',  f'url="{r["url"]}" AND chat_id="{r["chat_id"]}"')
-        else:
-            # It;s a first check or certificate did not changed
-            if r['cert_id'] == '0' or cert_id == r['cert_id']:
-                result = 'OK'
-            else:
-                result = 'Certificate was changed'
-                send_to_chat(r['chat_id'], f'{r["url"]} check certificate:\n{result}')
-            logging.debug(f'{result}')
-            servers_db.update(f'last_checked=CURRENT_TIMESTAMP, last_ok=CURRENT_TIMESTAMP, status="{escape_markdown(result)}", cert_id="{cert_id}"',  f'url="{r["url"]}" AND chat_id="{r["chat_id"]}"')
 
 if __name__ == '__main__':
     main()
