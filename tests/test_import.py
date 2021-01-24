2da1233333d15b44b9908806800c9f6d028be069
Sergey Matveychuk <sem@semmy.ru>
2021-01-24 18:49:20 +0300

    Add a basic test

---
+++ tests/test_import.py
@@ -0,0 +1,17 @@
+import pytest
+
+import check_certs_lib.cert_to_text
+import check_certs_lib.check_certs
+import check_certs_lib.check_validity
+import check_certs_lib.db
+import check_certs_lib.db_schemas
+import check_certs_lib.dns_requests
+import check_certs_lib.escape_markdown
+import check_certs_lib.get_cert_from_server
+import check_certs_lib.ocsp
+import check_certs_lib.send_to_chat
+import check_certs_lib.tlsa
+import check_certs_lib.verify_cert
+
+def test_import():
+    pass
