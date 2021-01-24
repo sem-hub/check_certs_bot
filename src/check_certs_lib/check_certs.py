4d27a80c539bec63c1005a95cc0f4b317095ec3c
Sergey Matveychuk <sem@semmy.ru>
2021-01-24 18:02:49 +0300

    Remove needless code from check_certs module

--- src/check_certs_lib/check_certs.py
+++ src/check_certs_lib/check_certs.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-
-import argparse
-import datetime
 import logging
-from pytz import UTC
-import ssl
 
 from check_certs_lib.check_validity import parse_and_check_url
 from check_certs_lib.get_cert_from_server import get_chain_from_server
@@ -133,50 +127,3 @@ def check_cert(url_str: str, flags: dict) -> str:
                         message = message + f'TLSA is *{res}*\n'
 
     return message
-
-# MAIN ()
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('url', nargs=1, help='protocol://hostname.domain:port')
-    parser.add_argument('-d', '--debug', action='store_true',
-                        help='Print much info for debugging')
-    parser.add_argument('-q', '--quiet', action='store_true',
-                        help='Print only error messages')
-    parser.add_argument('-id', '--print-id', action='store_true',
-                        help='Print certificate ID. Useful with --quiet')
-    parser.add_argument('-w', '--warn-before-expired', type=int, default=5,
-        help='Make a warning before certificate expired. Default: 5 days. 0 for off.')
-    parser.add_argument('-4', '--only-ipv4', action='store_true',
-                        help='Use only IPv4 addresses for checks')
-    parser.add_argument('-6', '--only-ipv6', action='store_true',
-                        help='Use only IPv6 addresses for checks')
-    parser.add_argument('-1', '--only-one', action='store_true',
-                        help='Use only first IP for checking')
-    parser.add_argument('--no-tlsa', action='store_true',
-                        help='Prevent TLSA checking')
-    parser.add_argument('--no-ocsp', action='store_true',
-                        help='Prevent OCSP checking')
-    args = parser.parse_args()
-    url = args.url[0]
-
-    flags = dict()
-    flags['quiet'] = args.quiet
-    flags['print_id'] = args.print_id
-    flags['warn_before_expired'] = args.warn_before_expired
-    flags['only_ipv4'] = args.only_ipv4
-    flags['only_ipv6'] = args.only_ipv6
-    flags['only_one'] = args.only_one
-    flags['no_tlsa'] = args.no_tlsa
-    flags['no_ocsp'] = args.no_ocsp
-
-    if args.debug:
-      logging.basicConfig(level=logging.DEBUG)
-    else:
-      logging.basicConfig(format='%(message)s', level=logging.INFO)
-
-    if '://' not in url:
-        url = 'https://' + url
-
-    logging.debug(f'url={url}')
-
-    print(check_cert(url, flags), end='')
