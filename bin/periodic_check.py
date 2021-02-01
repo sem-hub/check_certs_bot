47b5ca71103bc73837dcd17b52f30db09d7f8ee4
Sergey Matveychuk <sem@semmy.ru>
2021-02-02 01:47:37 +0300

    Do a smart checking. If we have a few users asked for one URL, check it only once. Send messages to all users who requestet this URL. Rename send_to_chat() to send_to_chats() to reflect symantic changes.

--- bin/periodic_check.py
+++ bin/periodic_check.py
@@ -9,7 +9,7 @@ from typing import NoReturn
 from check_certs_lib.check_certs import check_cert
 from check_certs_lib.db import DB_factory
 from check_certs_lib.logging_black_white_lists import Blacklist, add_filter_to_all_handlers
-from check_certs_lib.send_to_chat import send_to_chat
+from check_certs_lib.send_to_chat import send_to_chats
 
 def check_process_closure(db, dry_run: bool):
     global helper       # it's a dirty hack to prevent "Can't picle local object" error in multiprocessing module
@@ -28,6 +28,7 @@ def process_checking(db, dry_run, rt: tuple) -> dict:
     res['cert_id'] = r['cert_id']
     res['url'] = r['url']
     res['chat_id'] = r['chat_id']
+    res['count'] = r['count']
     res['error'], res['out_text'] = check_cert(
             r['url'],
             quiet=True,
@@ -41,6 +42,10 @@ def process_checking(db, dry_run, rt: tuple) -> dict:
 def process_results(servers_db, r: dict) -> NoReturn:
     if not r:
         return
+    # we have more than one user for this url
+    users = [r['chat_id']]
+    if r['count']:
+        users = [v['chat_id'] for v in servers_db.select('chat_id', f'url={r["url"]!r}')]
     if r['error']:
         result = r['out_text']+r['error']
     else:
@@ -50,29 +55,28 @@ def process_results(servers_db, r: dict) -> NoReturn:
     result = result.strip('\n')
     m = re.search('ID: ([0-9A-Z]+)\n?', result)
     if m is None:
-        send_to_chat(r['chat_id'], f'{r["url"]} check certificate error:\n{result}')
+        send_to_chats(f'{r["url"]} check certificate error:\n{result}', *users)
         logging.debug(f'Error: |{result}|')
-        servers_db.update(f'last_checked=CURRENT_TIMESTAMP, status={result!r}',
-                f'url={r["url"]!r} AND chat_id={r["chat_id"]!r}')
+        servers_db.update(f'last_checked=CURRENT_TIMESTAMP, status={result!r}', f'url={r["url"]!r}')
         return
     cert_id = m.group(1)
     result = re.sub('ID: ([0-9A-Z]+)\n?', '', result)
     if result != '':
-        send_to_chat(r['chat_id'], f'{r["url"]} check certificate error:\n{result}')
+        send_to_chats(f'{r["url"]} check certificate error:\n{result}', *users)
         logging.debug(f'Error*: {result}')
         servers_db.update(f'last_checked=CURRENT_TIMESTAMP, status={result!r}, cert_id={cert_id!r}',
-                f'url={r["url"]!r} AND chat_id={r["chat_id"]!r}')
+                f'url={r["url"]!r}')
     else:
         # It's a first check or certificate did not changed
         if r['cert_id'] == '0' or cert_id == r['cert_id']:
             result = 'OK'
         else:
             result = 'Certificate was changed'
-            send_to_chat(r['chat_id'], f'{r["url"]} check certificate:\n{result}')
+            send_to_chats(f'{r["url"]} check certificate:\n{result}', *users)
         logging.debug(f'{result}')
         servers_db.update(
                 f'last_checked=CURRENT_TIMESTAMP, last_ok=CURRENT_TIMESTAMP, status={result!r}, cert_id={cert_id!r}',
-                f'url={r["url"]!r} AND chat_id={r["chat_id"]!r}')
+                f'url={r["url"]!r}')
 
 def main():
     parser = argparse.ArgumentParser()
@@ -90,7 +94,7 @@ def main():
 
     db_factory = DB_factory()
     servers_db = db_factory.get_db('servers')
-    res = servers_db.select('*')
+    res = servers_db.select('*, COUNT(url) AS count', 'true GROUP BY url')
 
     proc_exec = check_process_closure(servers_db, args.dry_run)
     with Pool(processes=args.proc_num) as pool:
--- src/check_certs_lib/send_to_chat.py
+++ src/check_certs_lib/send_to_chat.py
@@ -1,11 +1,12 @@
 import logging
 import rpyc
 
-def send_to_chat(chat_id:int, msg: str):
+def send_to_chats(msg: str, chat_ids: list):
     logger = logging.getLogger(__name__)
     try:
         rcon = rpyc.connect('localhost', 18861)
-        rcon.root.add_message(chat_id, msg)
+        for chat_id in chat_ids:
+            rcon.root.add_message(chat_id, msg)
     except ConnectionError as error:
         logger.error(f'Connection to chat server error: {str(error)}')
         logger.info(msg)
