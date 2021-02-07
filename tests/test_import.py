97a886f3486e432c718dcb11f429fd45c24f0b8d
Sergey Matveychuk <sem@semmy.ru>
2021-02-07 16:20:21 +0300

    Fix tests. Split lines for better look.

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,17 +1,49 @@
 import pytest
 
-from check_certs_lib.cert_to_text import need_bold, need_italic, need_strike, need_code, need_pre, cert_to_text
+from check_certs_lib.cert_to_text import (
+        need_bold,
+        need_italic,
+        need_strike,
+        need_code,
+        need_pre,
+        cert_to_text,
+        datetime_to_user_tz_str
+        )
 from check_certs_lib.check_certs import check_cert
-from check_certs_lib.check_validity import is_valid_fqdn, parse_and_check_url 
-from check_certs_lib.db import DBfactory, DB
-from check_certs_lib.db_schemas import servers_create_statement, users_create_statement, activity_create_statement
-from check_certs_lib.dns_requests import check_fqdn, get_all_dns, get_dns_request, get_dnssec_request, get_tlsa_record
-from check_certs_lib.get_cert_from_server import get_chain_from_server, get_cert_from_server
-from check_certs_lib.logging_black_white_lists import Blacklist, Whitelist, add_filter_to_all_handlers
+from check_certs_lib.check_validity import (
+        is_valid_fqdn,
+        parse_and_check_url
+        )
+from check_certs_lib.db_model import (
+        Servers,
+        Users,
+        Activity,
+        DB
+        )
+from check_certs_lib.dns_requests import (
+        check_fqdn,
+        get_all_dns,
+        get_dns_request,
+        get_dnssec_request,
+        get_tlsa_record
+        )
+from check_certs_lib.get_cert_from_server import (
+        get_chain_from_server,
+        get_cert_from_server
+        )
+from check_certs_lib.logging_black_white_lists import (
+        Blacklist,
+        Whitelist,
+        add_filter_to_all_handlers
+        )
 from check_certs_lib.ocsp import check_ocsp
 from check_certs_lib.send_to_chat import send_to_chats
 from check_certs_lib.tlsa import check_tlsa, generate_tlsa
-from check_certs_lib.verify_cert import get_days_before_expired, get_domains_from_cert, verify_cert
+from check_certs_lib.verify_cert import (
+        get_days_before_expired,
+        get_domains_from_cert,
+        verify_cert
+        )
 
 def test_import():
     pass
