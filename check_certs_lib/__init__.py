9cc062538b8464d1d0bbfe125d6482f53831e821
Sergey Matveychuk <sem33@yandex-team.ru>
2022-12-19 20:53:48 +0300

    Bump version

--- check_certs_lib/__init__.py
+++ check_certs_lib/__init__.py
@@ -3,4 +3,4 @@ A library to work with SSL certificates, check them, makeing SSL connections,
 DNS requests (with DNSSEC checks on request),
 output the certifications as a text, etc.
 '''
-__version__ = '5.0.5'
+__version__ = '5.0.6'
