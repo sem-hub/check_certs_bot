9dcbdd5b39f5d6848826790f337ea6be79add71b
Sergey Matveychuk <sem33@yandex-team.ru>
2023-01-13 19:16:15 +0300

    Use @functools.cache decorator instead of own implementation

--- check_certs_lib/check_ipv6.py
+++ check_certs_lib/check_ipv6.py
@@ -1,23 +1,19 @@
 '''Check if IPv6 stack configured and work.'''
 
+from functools import cache
+
 from check_certs_lib.dns_requests import get_dns_request
 from check_certs_lib.get_cert_from_server import get_cert_from_server
 
-ipv6_work_cached = None
+@cache
 def check_ipv6_work() -> bool:
     '''Check if IPv6 stack configured and work'''
-    global ipv6_work_cached
-    if ipv6_work_cached is not None:
-        return ipv6_work_cached
-
-    yandex_ipv6 = get_dns_request('yandex.ru', 'AAAA')
+    yandex_ipv6 = get_dns_request('ya.ru', 'AAAA')
     if len(yandex_ipv6) == 0:
         return False
-    err, cert = get_cert_from_server('yandex.ru', str(yandex_ipv6[0]),
+    err, cert = get_cert_from_server('ya.ru', str(yandex_ipv6[0]),
             443, 'https')
     if err:
-        ipv6_work_cached = False
         return False
 
-    ipv6_work_cached = True
     return True
