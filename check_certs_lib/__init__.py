74f62789c951760ffb73515d6d5bb630bf9a99ae
Sergey Matveychuk <sem@semmy.ru>
2023-01-14 22:49:50 +0300

    Bump version. gitignore file with my notices about alembic

--- check_certs_lib/__init__.py
+++ check_certs_lib/__init__.py
@@ -3,7 +3,7 @@ A library to work with SSL certificates, check them, makeing SSL connections,
 DNS requests (with DNSSEC checks on request),
 output the certifications as a text, etc.
 '''
-__version__ = '5.0.6'
+__version__ = '5.0.7'
 
 # Check python version. We need 3.9+
 import sys
